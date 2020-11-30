27b9f5d9c25403ca4a7bd2151c682622ea77f1fa
habadbrame <habadbramed@gmail.com>
2020-11-30 09:47:34 -0600

    Navbar como componente

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,18 @@
 <template>
-  <div id="nav">  
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Fletes</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <router-link class="nav-link" to="/">Nosotros</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link class="nav-link" to="/Contactos">Contactos</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link class="nav-link" to="/Avisos">Avisos Legales</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link class="nav-link" to="/SoyClienteEnvia">Soy Cliente que Envia</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link class="nav-link" to="/SoyTransportista">Soy Transportista</router-link>
-            </li>
-            </ul>
-            <span class="navbar-text">
-            <div id="nav">
-                <router-link to="/login">Login</router-link> |
-                <router-link to="/register">Register</router-link> 
-            </div>
-            </span>
-        </div>
-    </nav>
-  </div>
-  <router-view/>
+  <Navbar />
 </template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'nav',
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 #app {
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div id="nav">  
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Fletes</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -7,34 +8,31 @@
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <router-link class="nav-link" to="/">Home</router-link> |
+                <router-link class="nav-link" to="/">Nosotros</router-link>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-                <router-link to="/Contactos">Contactos</router-link>
+                <router-link class="nav-link" to="/Contactos">Contactos</router-link>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-                <router-link to="/Avisos">Avisos Legales</router-link> |
+                <router-link class="nav-link" to="/Avisos">Avisos Legales</router-link>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-                <router-link to="/SoyClienteEnvia">Soy Cliente que Envia</router-link>
+                <router-link class="nav-link" to="/SoyClienteEnvia">Soy Cliente que Envia</router-link>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-                <router-link to="/SoyTransportista">Soy Transportista</router-link>
+                <router-link class="nav-link" to="/SoyTransportista">Soy Transportista</router-link>
             </li>
             </ul>
             <span class="navbar-text">
             <div id="nav">
-                <router-link to="/">Home</router-link> |
-                <router-link to="/Contactos">Contactos</router-link> 
+                <router-link to="/login">Login</router-link> |
+                <router-link to="/register">Register</router-link> 
             </div>
             </span>
         </div>
-        <router-view/>
     </nav>
+  </div>
+  <router-view/>
 </template>
 
 <script>
