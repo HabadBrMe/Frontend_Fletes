21a40d9ea2605bd347a5759cd163802318a5c9b3
Adrislpz <adrislpz01@gmail.com>
2020-12-15 17:45:42 -0600

    view Nosotros

--- src/views/SitioWebInfo/Nosotros.vue
+++ src/views/SitioWebInfo/Nosotros.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+
+
+<div class="row-md-12 p-5">
+
+
+      <img src="fletes.jpg"  style="max-width:600px; max-height: 400px; " alt="Fletes">
+<div class="col-md-12" style="width:100%;">
+              
+
+                <div class="left_text">
+                    <h2>¿Qué es Fletes?</h2>
+                </div>
+                <p style="text-align: justify !important;    font-size: 18px;
+    font-weight: 200;">Fletes, es la primera empresa en México para el servicio de gestión de transporte de carga, que funciona a través de una aplicación disponible para computadoras, laptops y celulares. </p>
+                <p style="text-align: justify !important;    font-size: 18px;
+    font-weight: 200;">
+                    Optimizamos, modernizamos y facilitamos la conexión entre transportistas y empresas generadoras de cargas, a través de comunidades de transporte. </p>
+      
+
+ <div class="card border-success" style="max-width: 100%; padding-top:20px">
+
+<h2 style="text-align: center;    font-size: 18px;
+    font-weight: 200;">Visión</h2>
+
+
+  <div class="card-header bg-transparent border-success" style="text-align: justify !important;    font-size: 18px;
+    font-weight: 200;">
+
+
+ <p class="card-text" style="text-align: justify !important;    font-size: 18px;
+    font-weight: 200;">Ser la plataforma virtual de intercambio de servicios de transporte más utlizada y segura en la región. Generando cada vez mayor valor agregado mediante servicios colaterales relacionados al transporte de carga.</p>
+  
+
   </div>
+
+  <h5 class="card-title" style="text-align: center;    font-size: 18px;
+    font-weight: 200;" >Nuestro propósito</h5>
+
+  <div class="card-body text-success;">
+   
+    <p class="card-text" style="text-align: justify !important;    font-size: 18px;
+    font-weight: 200;">Poner al alcance del mercado innovación y tecnología para facilitar las operaciones de transporte dentro de la cadena logística de abastecimiento, así como en los requerimientos ordinarios de la sociedad, de transporte de carga.</p>
+  </div>
+  <div class="card-footer bg-transparent border-success">
+
+
+  </div>
+
+
+  
+   </div>
+    
+         
+</div>
+
+
+  <div class="col-md-12 offset-md-4 px-5">
+
+    
+
+  </div>
+
+
+
+
+</div>
+
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
