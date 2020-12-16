4d0fdb2b8c9e2d5bd11c096e0959f92a96002350
Adrislpz <adrislpz01@gmail.com>
2020-12-16 13:04:52 -0600

    Calificaciones

--- src/router/index.js
+++ src/router/index.js
@@ -2,126 +2,149 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/SitioWebInfo/Nosotros.vue'
 import store from '@/store/index.js'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Nosotros',
-    component: Home
-  },
-  {
-    path: '/contactos',
-    name: 'Contactos',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/SitioWebInfo/Contactos.vue')
-  },
-  {
-    path: '/avisos',
-    name: 'Avisos',
-    component: () => import('../views/SitioWebInfo/AvisosLegales.vue')
-  },
-  {
-    path: '/soyclienteenvia',
-    name: 'ClienteEnvia',
-    component: () => import('../views/SitioWebInfo/SoyClienteEnvia.vue')
-  },
-  {
-    path: '/soytransportista',
-    name: 'SoyTransportista',
-    component: () => import('../views/SitioWebInfo/SoyTransportista.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/GestionUsuarios/Login.vue')
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import('../views/GestionUsuarios/Registro.vue')
-  },
-//----------------------Usuarios----------------------------------------------
-  {
-    path: '/ejemplo',
-    name: 'Tablero',
-    component: () => import('../views/Ejemplo.vue')
-  },
-  {
-    path: '/mensajes',
-    name: 'Mensajes',
-    component: () => import('../views/UserEnvia/Mensajes.vue'),
-    meta: { 
-      requiresAuth: true
-    }
-  },
-//-----------------------Usuario que envia------------------------------------------
-{
-  path: '/envios',
-  name: 'Envios',
-  component: () => import('../views/UserEnvia/Envios/PublicarEnvio.vue'),
-  meta: { 
-    requiresAuth: true,
-    requiresRol: "Cliente que envia" 
-   }
-},
-//-----------------------Usuario transportista--------------------------------------
-{
-  path: '/vehiculos',
-  name: 'Vehiculos',
-  component: () => import('../views/UsuarioTransportista/moduloVehiculo/Vehiculos.vue'),
-  meta: { 
-    requiresAuth: true,
-    requiresRol: "Transportista" 
-   }
-},
-{
-  path: '/choferes',
-  name: 'Choferes',
-  component: () => import('../views/UsuarioTransportista/moduloChofer/Choferes.vue'),
-  meta: { 
-    requiresAuth: true,
-    requiresRol: "Transportista"
-   }
-},
-//-----------------------Usuario monitorista----------------------------------------
-{
-  path: '/monitoreo',
-  name: 'Monitoreo',
-  component: () => import('../views/UserEnvia/Mensajes.vue'),
-  meta: { 
-    requiresAuth: true,
-    requiresRol: "Monitorista"
-  }
-},
-{
-  path: '/historial',
-  name: 'Historial',
-  component: () => import('../views/UserEnvia/Mensajes.vue'),
-  meta: { 
-    requiresAuth: true,
-    requiresRol: "Monitorista"
-  }
-},
+const routes = [{
+        path: '/',
+        name: 'Nosotros',
+        component: Home
+    },
+    {
+        path: '/contactos',
+        name: 'Contactos',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/SitioWebInfo/Contactos.vue')
+    },
+    {
+        path: '/avisos',
+        name: 'Avisos',
+        component: () =>
+            import ('../views/SitioWebInfo/AvisosLegales.vue')
+    },
+    {
+        path: '/soyclienteenvia',
+        name: 'ClienteEnvia',
+        component: () =>
+            import ('../views/SitioWebInfo/SoyClienteEnvia.vue')
+    },
+    {
+        path: '/soytransportista',
+        name: 'SoyTransportista',
+        component: () =>
+            import ('../views/SitioWebInfo/SoyTransportista.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () =>
+            import ('../views/GestionUsuarios/Login.vue')
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: () =>
+            import ('../views/GestionUsuarios/Registro.vue')
+    },
+    //----------------------Usuarios----------------------------------------------
+    {
+        path: '/ejemplo',
+        name: 'Tablero',
+        component: () =>
+            import ('../views/Ejemplo.vue')
+    },
+    {
+        path: '/mensajes',
+        name: 'Mensajes',
+        component: () =>
+            import ('../views/UserEnvia/Mensajes.vue'),
+        meta: {
+            requiresAuth: true
+        }
+    },
+    //-----------------------Usuario que envia------------------------------------------
+    {
+        path: '/envios',
+        name: 'Envios',
+        component: () =>
+            import ('../views/UserEnvia/Envios/PublicarEnvio.vue'),
+        meta: {
+            requiresAuth: true,
+            requiresRol: "Cliente que envia"
+        }
+    },
+
+    {
+        path: '/calificaciones',
+        name: 'Calificaciones',
+        component: () =>
+            import ('../views/UserEnvia/Envios/Calificaciones/CalificarTransportista.vue'),
+        meta: {
+            requiresAuth: true,
+
+        }
+    },
+    //-----------------------Usuario transportista--------------------------------------
+    {
+        path: '/vehiculos',
+        name: 'Vehiculos',
+        component: () =>
+            import ('../views/UsuarioTransportista/moduloVehiculo/Vehiculos.vue'),
+        meta: {
+            requiresAuth: true,
+            requiresRol: "Transportista"
+        }
+    },
+    {
+        path: '/choferes',
+        name: 'Choferes',
+        component: () =>
+            import ('../views/UsuarioTransportista/moduloChofer/Choferes.vue'),
+        meta: {
+            requiresAuth: true,
+            requiresRol: "Transportista"
+        }
+    },
+    //-----------------------Usuario monitorista----------------------------------------
+    {
+        path: '/monitoreo',
+        name: 'Monitoreo',
+        component: () =>
+            import ('../views/UserEnvia/Mensajes.vue'),
+        meta: {
+            requiresAuth: true,
+            requiresRol: "Monitorista"
+        }
+    },
+    {
+        path: '/historial',
+        name: 'Historial',
+        component: () =>
+            import ('../views/UserEnvia/Mensajes.vue'),
+        meta: {
+            requiresAuth: true,
+            requiresRol: "Monitorista"
+        }
+    },
 
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    if (localStorage.Uinfo && localStorage.Utoken && localStorage.Urole) {
-      store.commit('saveUser')
-      next()
-    }else {
-      next('/login')
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (localStorage.Uinfo && localStorage.Utoken && localStorage.Urole) {
+            store.commit('saveUser')
+            next()
+        } else {
+            next('/login')
+        }
+    } else {
+        next()
     }
-  } else {
-    next()
-  }
 })
 
 export default router
---
+++ src/views/UserEnvia/Envios/Calificaciones/CalificarTransportista.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="about">
+    <h1>This is an Soy Cliente Envia page</h1>
+    
+<h2>Star Rating</h2>
+<span class="start fa fa-star " id="1"></span>
+<span class="start fa fa-star " id="2"></span>
+<span class="start fa fa-star " id="3"></span>
+<span class="start fa fa-star"  id="4"></span>
+<span class="start fa fa-star"  id="5"></span>
+<h2 id="showRating" ></h2>
+
+  </div>
+</template>
+
+<style>
+.checked {
+  color: orange;
+}
+</style>
+
+<script>
+import $ from 'jquery'
+
+export default {
+  mounted() {
+      $('.start').on('click', (e) => {
+
+        //OBTENER la clase y buscar si tiene cheked
+       //si no tiene cheked marcar checked 
+       //si tiene checked, borrar checked
+       //crear una variable contadora de checked o un checked
+       var idData=  e.target.id;
+       let count=idData;
+       let countMore=idData;
+      
+       do{
+          console.log(count)
+          $("#"+countMore).removeClass("checked");
+          countMore++;
+        }
+        while(countMore<=5);
+      
+      do{
+          console.log(count)
+          $("#"+count).addClass("checked");
+          count--;
+        }
+        while(count>0);
+        
+      $("#showRating").html(idData)
+      
+      });
+   },
+   methods: {
+      logData(id) {
+         console.log(id);
+      },
+   }
+}
+</script>
+
